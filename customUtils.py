116b20b288da80e495c03dab7c285305d693b2bc
Angeleah <a.hoeppner@yahoo.ca>
2023-05-23 10:32:50 -0700

    Added save_list, updated def name save -> save_gam

--- customUtils.py
+++ customUtils.py
@@ -1,15 +1,17 @@
 import sys
 
+
 def getAns(ans, options):
     while ans not in options:
         print(f"You must choose one of the following: {options}")
         ans = input()
     return ans
 
-def save(game, score):
+
+def save_game(game, score):
     fileName = "scores.txt"
     print("Would you like to save your score? [y/n]")
-    ans = getAns(input(), ['y','n'])
+    ans = getAns(input(), ['y', 'n'])
     if ans == 'y':
         print("Please enter your name")
         name = input()
@@ -17,6 +19,19 @@ def save(game, score):
         file.writelines(f"{game}:: {name}: {score}\n")
         file.close()
         sortFile(fileName)
+
+
+def save_list(items):
+    print("Would you like to save your list? [y/n]")
+    ans = getAns(input(), ['y', 'n'])
+    if ans == 'y':
+        print("Please enter a name for your list: ", end="")
+        name = input()
+        file = open(f"{name}.txt", "a+")
+        for item in items:
+            file.write(f"{item}\n")
+        file.close()
+
 
 def sortFile(fileName):
     with open(fileName, 'r') as reader:
@@ -28,8 +43,9 @@ def sortFile(fileName):
     writeFile.writelines(lines)
     writeFile.close() """
 
+
 def playAgain(game, score):
     print("Would you like to play again? [y/n]")
-    if getAns(input(), ['y','n']) == 'n':
-        save(game, score)
+    if getAns(input(), ['y', 'n']) == 'n':
+        save_game(game, score)
         sys.exit(0)
